7a89440338556a2a400af10fcfde647b9c627996
Siddhanth10 <siddhanthsukhdeve.ml24@sbjit.edu.in>
2026-03-31 12:33:48 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -200,6 +200,8 @@ app.get("/my-bookings/:user_id", (req, res) => {
 
 
 // ================= START =================
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
